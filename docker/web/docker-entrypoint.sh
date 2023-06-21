962eaccb49a2222fafb8e8dfc69dc01c42ba6172
Huy <macbook@Macbooks-MacBook-Pro.local>
2023-06-21 22:43:05 +0700

    first commit

---
+++ docker/web/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Exit on fail
+set -e
+
+# Bundle install
+gem install bundler -v '2.4.7'
+bundle install --jobs=4
+
+# Waiting for dependent containers
+/wait-for-it.sh db:3306
+
+# Migrate
+bundle exec rake db:create
+bundle exec rake db:migrate
+
+# Remove puma pid if existed
+rm -f tmp/pids/server.pid
+
+# Start services
+
+bundle exec rails server --port=3000 -b 0.0.0.0
+
+# Finally call command issued to the docker service
+exec "$@"
